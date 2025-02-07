57574a3d67fa3d70e16ea64d742c032562ee7601
trinhdinhtai <taitd153.dev@gmail.com>
2025-02-07 18:07:29 +0700

    feat: add FlipLink component and integrate into navbar

---
+++ components/flip-link.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+const DURATION = 0.25
+const STAGGER = 0.025
+
+interface Props {
+  children: string
+  href: string
+}
+
+const FlipLink = ({ children, href }: Props) => {
+  return (
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      href={href}
+      className="relative block overflow-hidden whitespace-nowrap text-sm font-light uppercase"
+      style={{
+        lineHeight: 0.75,
+      }}
+    >
+      <div>
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-100%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "100%",
+              },
+              hovered: {
+                y: 0,
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.a>
+  )
+}
+
+export default FlipLink
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,3 +1,4 @@
+import FlipLink from "@/components/flip-link"
 import Logo from "@/components/logo"
 
 export default function Navbar() {
@@ -5,6 +6,12 @@ export default function Navbar() {
     <nav>
       <div className="relative flex items-center gap-5 px-4 pb-3">
         <Logo />
+        <div className="hidden gap-4 lg:flex">
+          <FlipLink href="/travel">Travel</FlipLink>
+          <FlipLink href="/discover">Discover</FlipLink>
+          <FlipLink href="/blog">Blog</FlipLink>
+          <FlipLink href="/about">About</FlipLink>
+        </div>
       </div>
     </nav>
   )
